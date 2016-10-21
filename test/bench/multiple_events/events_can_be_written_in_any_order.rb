edcd72661fd6d82520dec6b50e650f14a1d52eaf
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2016-10-21 09:58:53 -0500

    Expecting multiple events with block provided is tested

--- test/bench/multiple_events/events_can_be_written_in_any_order.rb
+++ test/bench/multiple_events/events_can_be_written_in_any_order.rb
@@ -2,35 +2,51 @@ require_relative '../bench_init'
 
 context "Multiple events, events can be written in any order" do
   context "Event types specified match event types written" do
+    context do
+      event_types = Fixtures::ExpectMessage::Controls::EventTypes.example
+
+      stream_name = Fixtures::ExpectMessage::Controls::Write.(event_types)
+      expect_message = Fixtures::ExpectMessage::Controls::ExpectMessage.example stream_name
+
+      test "Returns true" do
+        assert expect_message.(event_types, any_order: true)
+      end
+    end
+
+    context "Block is provided and predicate returns true" do
+      event_types = Fixtures::ExpectMessage::Controls::EventTypes.example
+
+      stream_name = Fixtures::ExpectMessage::Controls::Write.(event_types)
+      expect_message = Fixtures::ExpectMessage::Controls::ExpectMessage.example stream_name
+
+      test "Returns true" do
+        assert expect_message.(event_types, any_order: true) {|_| true}
+      end
+    end
+
+    context "Block is provided and predicate returns false" do
+      event_types = Fixtures::ExpectMessage::Controls::EventTypes.example
+
+      stream_name = Fixtures::ExpectMessage::Controls::Write.(event_types)
+      expect_message = Fixtures::ExpectMessage::Controls::ExpectMessage.example stream_name
+
+    test "Error is raised" do
+        assert proc { expect_message.(event_types, any_order: true) {|_| false} } do
+          raises_error? Fixtures::ExpectMessage::ExpectationNotMet
+        end
+      end
+    end
+  end
+
+  context "Event types specified do not match event types written" do
     event_types = Fixtures::ExpectMessage::Controls::EventTypes.example
 
     stream_name = Fixtures::ExpectMessage::Controls::Write.(event_types)
     expect_message = Fixtures::ExpectMessage::Controls::ExpectMessage.example stream_name
 
-    test "Returns true" do
-      assert expect_message.(event_types, any_order: true)
-    end
-  end
-end
-__END__
-
-    context "Block is provided and predicate returns true" do
-      stream_name = Fixtures::ExpectMessage::Controls::Write.()
-      expect_message = Fixtures::ExpectMessage::Controls::ExpectMessage.example stream_name
-
-      test "Returns true" do
-        assert expect_message.(event_type) {|_| true}
-      end
-    end
-
-    context "Block is provided and predicate returns false" do
-      stream_name = Fixtures::ExpectMessage::Controls::Write.()
-      expect_message = Fixtures::ExpectMessage::Controls::ExpectMessage.example stream_name
-
-      test "Error is raised" do
-        assert proc { expect_message.(event_type) {|_| false} } do
-          raises_error? Fixtures::ExpectMessage::ExpectationNotMet
-        end
+    test "Error is raised" do
+      assert proc { expect_message.(['WrongEventType', 'OtherWrongEventType'], any_order: true) } do
+        raises_error? Fixtures::ExpectMessage::ExpectationNotMet
       end
     end
   end
